dd82feb2a32533cd3c765f00cf961dc4ae9c74ac
Divyanshu Kumar <divyanshuk10@gmail.com>
2022-11-08 11:54:28 +0530

    setting up kover

--- app/src/test/java/com/example/mytestingapp/ui/main/MainActivityViewModelTest.kt
+++ app/src/test/java/com/example/mytestingapp/ui/main/MainActivityViewModelTest.kt
@@ -1,28 +1,31 @@
 package com.example.mytestingapp.ui.main
 
-import org.junit.After
+import android.support.test.runner.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+@RunWith(JUnit4::class)
 class MainActivityViewModelTest {
 
-  lateinit var CUT: MainActivityViewModel
+  //lateinit var CUT: MainActivityViewModel
 
-  @Before
-  fun setUp() {
-    CUT = MainActivityViewModel()
-  }
+//  @Before
+//  fun setUp() {
+//  }
 
-  @After
+  /*@After
   fun tearDown() {
-  }
+  }*/
 
   @Test
-  fun getNum1() {
-    CUT.num1.value = 10
-
+  fun `num1 is set to 10`() {
+    val i = 10
+    assertThat(i).isEqualTo(10)
   }
-
+/*
   @Test
   fun getNum2() {
   }
@@ -41,5 +44,5 @@ class MainActivityViewModelTest {
 
   @Test
   fun calculate() {
-  }
+  }*/
 }
